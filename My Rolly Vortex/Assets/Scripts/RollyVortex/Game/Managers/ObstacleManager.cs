7a675d195aa5fb6f395f0ab3120c6ce338ed87ff
Akash Basu <akashbasu1992@gmail.com>
2020-08-29 15:57:01 +0530

    Shuffle bag and obstacles

--- My Rolly Vortex/Assets/Scripts/RollyVortex/Game/Managers/ObstacleManager.cs
+++ My Rolly Vortex/Assets/Scripts/RollyVortex/Game/Managers/ObstacleManager.cs
@@ -8,6 +8,8 @@ namespace RollyVortex
         private static int _obstacleId = -1;
         private static IntRangedValue _groupRange;
 
+        private static ShuffleBag _shuffleBag;
+
         public void Initialize(Action<IInitializable> onComplete = null, params object[] args)
         {
             GameEventManager.Subscribe(GameEvents.LevelEvents.Start, OnLevelStart);
@@ -20,8 +22,10 @@ namespace RollyVortex
             _obstacleGroupCount--;
             if (_obstacleGroupCount <= 0 || _obstacleId < 0)
             {
-                _obstacleId = DeterministicRandomProvider.Next(0, ObstacleDataProvider.ObstacleData.Count - 1);
-                _obstacleGroupCount = DeterministicRandomProvider.Next(_groupRange.Min, _groupRange.Max) - 1;
+                _obstacleId = _shuffleBag.Next();
+                
+                _obstacleGroupCount = DeterministicRandomProvider.Next(_groupRange);
+                _obstacleGroupCount--;
             }
 
             return ObstacleDataProvider.ObstacleData[_obstacleId];
@@ -32,6 +36,8 @@ namespace RollyVortex
             _groupRange = LevelDataProvider.LevelData.Grouping;
             _obstacleGroupCount = -1;
             _obstacleId = -1;
+
+            _shuffleBag = new ShuffleBag(ObstacleDataProvider.ObstacleData.Count);
         }
     }
 }
