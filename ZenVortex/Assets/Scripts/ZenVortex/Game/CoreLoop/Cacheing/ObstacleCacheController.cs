ada7889b5984b7a6ad2a8d762e9ccb3f7d791e49
Akash Basu <akashbasu1992@gmail.com>
2020-10-11 17:44:47 +0530

    [DI] Caching

--- ZenVortex/Assets/Scripts/ZenVortex/Game/CoreLoop/Cacheing/ObstacleCacheController.cs
+++ ZenVortex/Assets/Scripts/ZenVortex/Game/CoreLoop/Cacheing/ObstacleCacheController.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
+using ZenVortex.DI;
 
 namespace ZenVortex
 {
     internal sealed class ObstacleCacheController : CacheController
     {
-        public ObstacleCacheController(Transform cache, Vector3 reCacheMarker) : base(cache, reCacheMarker, typeof(ObstacleController)) { }
-        protected override object[] GetSpawnData() => new object[] {ObstacleManager.GetNextObstacleData(), LevelDataProvider.LevelData};
-        protected override object[] GetFireData() => new object[] {ObstacleManager.GetCurrentGroupColor};
+        [Dependency] private readonly ObstacleManager _obstacleManager;
+        [Dependency] private readonly LevelDataProvider _levelDataProvider;
+        
+        public ObstacleCacheController(Transform cache, Vector3 reCacheMarker) : base(cache, reCacheMarker,
+            typeof(ObstacleController))
+        {
+            Injector.Inject(this);
+        }
+        
+        protected override object[] GetSpawnData() => new object[] {_obstacleManager.GetNextObstacleData(), _levelDataProvider.LevelData};
+        protected override object[] GetFireData() => new object[] {_obstacleManager.GetCurrentGroupColor};
     }
 }
--- ZenVortex/Assets/Scripts/ZenVortex/Game/CoreLoop/Cacheing/PowerupCacheController.cs
+++ ZenVortex/Assets/Scripts/ZenVortex/Game/CoreLoop/Cacheing/PowerupCacheController.cs
@@ -1,19 +1,28 @@
 using UnityEngine;
+using ZenVortex.DI;
 
 namespace ZenVortex
 {
     internal sealed class PowerupCacheController : CacheController
     {
-        internal PowerupCacheController(Transform cache, Vector3 reCacheMarker) : base(cache, reCacheMarker, typeof(PowerupController)) { }
+        [Dependency] private readonly DeterministicRandomProvider _deterministicRandomProvider;
+        [Dependency] private readonly PowerupManager _powerupManager;
+        [Dependency] private readonly LevelDataProvider _levelDataProvider;
+
+        internal PowerupCacheController(Transform cache, Vector3 reCacheMarker) : base(cache, reCacheMarker,
+            typeof(PowerupController))
+        {
+            Injector.Inject(this);
+        }
 
         public override void SpawnNext(float timeToTween)
         {
-            if(!DeterministicRandomProvider.NextBool(LevelDataProvider.LevelData.PowerupDropProbability)) return;
+            if(!_deterministicRandomProvider.NextBool(_levelDataProvider.LevelData.PowerupDropProbability)) return;
             
             base.SpawnNext(timeToTween);
         }
 
-        protected override object[] GetSpawnData() => new object[] {PowerupManager.GetNextPowerupData()};
+        protected override object[] GetSpawnData() => new object[] {_powerupManager.GetNextPowerupData()};
         protected override object[] GetFireData() => null;
     }
 }
