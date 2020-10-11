333e983f0d52461405afeb6c8d4adef3cb42eee7
Akash Basu <akashbasu1992@gmail.com>
2020-10-11 19:11:25 +0530

    Update GameEventManager.cs

--- ZenVortex/Assets/Scripts/ZenVortex/Game/Managers/GameEventManager.cs
+++ ZenVortex/Assets/Scripts/ZenVortex/Game/Managers/GameEventManager.cs
@@ -3,21 +3,12 @@ using System.Collections.Generic;
 
 namespace ZenVortex
 {
-    internal class GameEventManager : IInitializable
+    internal class GameEventManager
     {
-        private static Dictionary<string, List<Action<object[]>>> _eventDirectory;
+        private readonly Dictionary<string, List<Action<object[]>>> _eventDirectory = new Dictionary<string, List<Action<object[]>>>();
 
-        public void Initialize(Action<IInitializable> onComplete = null, params object[] args)
+        public void Subscribe(string gameEvent, Action<object[]> callback)
         {
-            _eventDirectory = new Dictionary<string, List<Action<object[]>>>();
-
-            onComplete?.Invoke(this);
-        }
-
-        public static void Subscribe(string gameEvent, Action<object[]> callback)
-        {
-            if (_eventDirectory == null) return;
-
             if (_eventDirectory.ContainsKey(gameEvent))
                 _eventDirectory[gameEvent].RemoveAll(x => x == callback);
             else
@@ -26,20 +17,16 @@ namespace ZenVortex
             _eventDirectory[gameEvent].Add(callback);
         }
 
-        public static void Unsubscribe(string gameEvent, Action<object[]> callback)
+        public void Unsubscribe(string gameEvent, Action<object[]> callback)
         {
-            if (_eventDirectory == null) return;
-
             if (!_eventDirectory.ContainsKey(gameEvent)) return;
 
             var callbacks = _eventDirectory[gameEvent];
             callbacks?.RemoveAll(x => x == callback);
         }
 
-        public static void Broadcast(string gameEvent, params object[] args)
+        public void Broadcast(string gameEvent, params object[] args)
         {
-            if (_eventDirectory == null) return;
-
             if (!_eventDirectory.ContainsKey(gameEvent)) return;
             
             if(_eventDirectory[gameEvent] == null) return;
