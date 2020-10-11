0a784ad849c17e1e31c86e82178be4cdedb9d890
Akash Basu <akashbasu1992@gmail.com>
2020-10-11 21:03:49 +0530

    Resource loader system

---
+++ ZenVortex/Assets/Scripts/ZenVortex/Game/AssetManagement/ResourcesLoader.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace ZenVortex
+{
+    internal class ResourcesLoader<TResource> where TResource : Object
+    {
+        private readonly string _resourcePath;
+
+        public ResourcesLoader(string resourcePath)
+        {
+            _resourcePath = resourcePath;
+        }
+        
+        public bool TryLoadData(out TResource[] resource)
+        {
+            if (LoadDataFromDisk(out resource)) return true;
+
+            Debug.LogError($"{nameof(GetType)} {nameof(TryLoadData)} failed to load data of type <{nameof(TResource)}> from disk! Path {_resourcePath}");
+            return false;
+        }
+        
+        private bool LoadDataFromDisk(out TResource[] resource)
+        {
+            resource =  Resources.LoadAll<TResource>(_resourcePath);
+            return resource.Length > 0;
+        }
+    }
+}
