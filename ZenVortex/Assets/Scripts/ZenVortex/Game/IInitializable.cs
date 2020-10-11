c105428d69ef18b43fdcc8c4afa4151e4e44e5b3
Akash Basu <akashbasu1992@gmail.com>
2020-10-11 21:34:07 +0530

    Remove duplicate IInitializable

--- ZenVortex/Assets/Scripts/ZenVortex/Game/IInitializable.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ZenVortex
-{
-    internal interface IInitializable
-    {
-        void Initialize(Action<IInitializable> onComplete = null, params object[] args);
-    }
-}
